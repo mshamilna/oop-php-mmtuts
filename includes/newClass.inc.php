d7a78cff8162b470013a0b016e06e251112a57d5
shamil <mshamilna@gmail.com>
2020-05-22 00:27:40 +0530

    how to delete an object

--- includes/newClass.inc.php
+++ includes/newClass.inc.php
@@ -3,11 +3,28 @@
   class NewClass {
     // properties and methods go here.
 
-    public $info = "this is some info.";
+    public $data = "this is some info. <br/>";
+
+    public function __construct(){
+      echo 'this class has been instantiated <br />';
+    }
+
+    public function setProperty($newData){
+      $this -> data = $newData;
+    }
+
+    public function getProperty(){
+      return $this -> data;
+    }
+
+    public function __destruct() {
+      echo '<br> this is frome destruct';
+    }
+
 
   }
 
-  $newObject = new NewClass;
-  var_dump($newObject);
+  // $newObject = new NewClass;
+  // var_dump($newObject);
 
 ?>
